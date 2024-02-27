51906fb1dc475feca27ce3bdc128ab2410c5d4af
ryanren <wenkun.ren@gmail.com>
2024-02-27 09:56:39 -0600

    rb files finished

---
+++ article.rb
@@ -0,0 +1,12 @@
+class Article
+  attr_accessor :attr_writer, :attr_reader
+
+  def initialize(attr_writer, attr_reader)
+    @attr_writer = attr_writer
+    @attr_reader = attr_reader
+  end 
+
+  def to_s
+    "#{@attr_reader} selected #{@attr_writer}"
+  end 
+end
